15164c92e82b72d7f6a6e3d89cb7dd0fcd35e412
MoJingHui <369188955@qq.com>
2025-08-29 19:51:48 +0800

    feat: AI 助手前端

--- apps/web-assistant/src/app/page.tsx
+++ apps/web-assistant/src/app/page.tsx
@@ -1,95 +1,541 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+import { StreamProvider, useStreamContext } from '@/providers/Stream';
+import { ThreadProvider } from '@/providers/Thread';
+import {
+  AppstoreAddOutlined,
+  CloseOutlined,
+  CloudUploadOutlined,
+  CommentOutlined,
+  CopyOutlined,
+  DislikeOutlined,
+  LikeOutlined,
+  OpenAIFilled,
+  PaperClipOutlined,
+  PlusOutlined,
+  ProductOutlined,
+  ReloadOutlined,
+  ScheduleOutlined,
+} from '@ant-design/icons';
+import {
+  Attachments,
+  type AttachmentsProps,
+  Bubble,
+  Conversations,
+  Prompts,
+  Sender,
+  Suggestion,
+  Welcome,
+  useXAgent,
+  useXChat,
+} from '@ant-design/x';
+import type { Conversation } from '@ant-design/x/es/conversations';
+import { Message } from '@langchain/langgraph-sdk';
+import { Button, GetProp, GetRef, Image, Popover, Space, Spin, message } from 'antd';
+import { createStyles } from 'antd-style';
+import dayjs from 'dayjs';
+import React, { useEffect, useRef, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+type BubbleDataType = {
+  role: string;
+  content: string;
+};
+
+const MOCK_SESSION_LIST = [
+  {
+    key: '5',
+    label: 'New session',
+    group: 'Today',
+  },
+  {
+    key: '4',
+    label: 'What has Ant Design X upgraded?',
+    group: 'Today',
+  },
+  {
+    key: '3',
+    label: 'New AGI Hybrid Interface',
+    group: 'Today',
+  },
+  {
+    key: '2',
+    label: 'How to quickly install and import components?',
+    group: 'Yesterday',
+  },
+  {
+    key: '1',
+    label: 'What is Ant Design X?',
+    group: 'Yesterday',
+  },
+];
+const MOCK_SUGGESTIONS = [
+  { label: 'Write a report', value: 'report' },
+  { label: 'Draw a picture', value: 'draw' },
+  {
+    label: 'Check some knowledge',
+    value: 'knowledge',
+    icon: <OpenAIFilled />,
+    children: [
+      { label: 'About React', value: 'react' },
+      { label: 'About Ant Design', value: 'antd' },
+    ],
+  },
+];
+const MOCK_QUESTIONS = [
+  'What has Ant Design X upgraded?',
+  'What components are in Ant Design X?',
+  'How to quickly install and import components?',
+];
+const AGENT_PLACEHOLDER = 'Generating content, please wait...';
+
+const useCopilotStyle = createStyles(({ token, css }) => {
+  return {
+    copilotChat: css`
+      height: 100vh;
+      display: flex;
+      flex-direction: column;
+      background: ${token.colorBgContainer};
+      color: ${token.colorText};
+    `, 
+    // chatHeader 样式
+    chatHeader: css`
+      height: 52px;
+      box-sizing: border-box;
+      border-bottom: 1px solid ${token.colorBorder};
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 0 10px 0 16px;
+    `,
+    headerTitle: css`
+      font-weight: 600;
+      font-size: 15px;
+    `,
+    headerButton: css`
+      font-size: 18px;
+    `,
+    conversations: css`
+      width: 300px;
+      .ant-conversations-list {
+        padding-inline-start: 0;
+      }
+    `,
+    // chatList 样式
+    chatList: css`
+      overflow: auto;
+      padding-block: 16px;
+      flex: 1;
+    `,
+    chatWelcome: css`
+      margin-inline: 16px;
+      padding: 12px 16px;
+      border-radius: 2px 12px 12px 12px;
+      background: ${token.colorBgTextHover};
+      margin-bottom: 16px;
+    `,
+    loadingMessage: css`
+      background-image: linear-gradient(90deg, #ff6b23 0%, #af3cb8 31%, #53b6ff 89%);
+      background-size: 100% 2px;
+      background-repeat: no-repeat;
+      background-position: bottom;
+    `,
+    // chatSend 样式
+    chatSend: css`
+      padding: 12px;
+    `,
+    sendAction: css`
+      display: flex;
+      align-items: center;
+      margin-bottom: 12px;
+      gap: 8px;
+    `,
+    speechButton: css`
+      font-size: 18px;
+      color: ${token.colorText} !important;
+    `,
+  };
+});
+
+interface CopilotProps {
+  copilotOpen: boolean;
+  setCopilotOpen: (open: boolean) => void;
+}
+
+
+const Chat = () => {
+  const [copilotOpen, setCopilotOpen] = useState(true);
+  const { styles } = useCopilotStyle();
+  const attachmentsRef = useRef<GetRef<typeof Attachments>>(null);
+  const abortController = useRef<AbortController>(null);
+
+  // ==================== State ====================
+
+  const [messageHistory, setMessageHistory] = useState<Record<string, any>>({});
+
+  const [sessionList, setSessionList] = useState<Conversation[]>(MOCK_SESSION_LIST);
+  const [curSession, setCurSession] = useState(sessionList[0].key);
+
+  const [attachmentsOpen, setAttachmentsOpen] = useState(false);
+  const [files, setFiles] = useState<GetProp<AttachmentsProps, 'items'>>([]);
+
+  const [inputValue, setInputValue] = useState('');
+
+  /**
+   * 🔔 Please replace the BASE_URL, PATH, MODEL, API_KEY with your own values.
+   */
+
+  // ==================== Runtime ====================
+
+  const stream = useStreamContext();
+  const [agent] = useXAgent<BubbleDataType>({
+    request: (info, callbacks) => {
+      stream.submit(
+        { messages: [info.message] },
+        {
+          streamMode: ["values"],
+          optimisticValues: (prev) => ({
+            ...prev,
+            messages: [
+              ...(prev.messages ?? []),
+              info.messages,
+            ],
+          }),
+        },
+      );
+      console.log("🚀 ~ Home ~ info, callbacks:", info, callbacks)
+    }
+  });
+
+  const loading = agent.isRequesting();
+
+  const { messages, onRequest, setMessages } = useXChat({
+    agent,
+    requestFallback: (_, { error }) => {
+      if (error.name === 'AbortError') {
+        return {
+          content: 'Request is aborted',
+          role: 'assistant',
+        };
+      }
+      return {
+        content: 'Request failed, please try again!',
+        role: 'assistant',
+      };
+    },
+    transformMessage: (info) => {
+      const { originMessage, chunk } = info || {};
+      let currentContent = '';
+      let currentThink = '';
+      try {
+        if (chunk?.data && !chunk?.data.includes('DONE')) {
+          const message = JSON.parse(chunk?.data);
+          currentThink = message?.choices?.[0]?.delta?.reasoning_content || '';
+          currentContent = message?.choices?.[0]?.delta?.content || '';
+        }
+      } catch (error) {
+        console.error(error);
+      }
+
+      let content = '';
+
+      if (!originMessage?.content && currentThink) {
+        content = `<think>${currentThink}`;
+      } else if (
+        originMessage?.content?.includes('<think>') &&
+        !originMessage?.content.includes('</think>') &&
+        currentContent
+      ) {
+        content = `${originMessage?.content}</think>${currentContent}`;
+      } else {
+        content = `${originMessage?.content || ''}${currentThink}${currentContent}`;
+      }
+
+      return {
+        content: content,
+        role: 'assistant',
+      };
+    },
+    resolveAbortController: (controller) => {
+      abortController.current = controller;
+    },
+  });
+
+  // ==================== Event ====================
+  const handleUserSubmit = (val: string) => {
+   
+    onRequest({
+      stream: true,
+      message: { id: uuidv4(), type: "human", content: val },
+    });
+
+    // session title mock
+    if (sessionList.find((i) => i.key === curSession)?.label === 'New session') {
+      setSessionList(
+        sessionList.map((i) => (i.key !== curSession ? i : { ...i, label: val?.slice(0, 20) })),
+      );
+    }
+  };
+
+  const onPasteFile = (_: File, files: FileList) => {
+    for (const file of files) {
+      attachmentsRef.current?.upload(file);
+    }
+    setAttachmentsOpen(true);
+  };
+
+  // ==================== Nodes ====================
+  const chatHeader = (
+    <div className={styles.chatHeader}>
+      <div className={styles.headerTitle}>✨ AI Copilot</div>
+      <Space size={0}>
+        <Button
+          type="text"
+          icon={<PlusOutlined />}
+          onClick={() => {
+            if (messages?.length) {
+              const timeNow = dayjs().valueOf().toString();
+              abortController.current?.abort();
+              // The abort execution will trigger an asynchronous requestFallback, which may lead to timing issues.
+              // In future versions, the sessionId capability will be added to resolve this problem.
+              setTimeout(() => {
+                setSessionList([
+                  { key: timeNow, label: 'New session', group: 'Today' },
+                  ...sessionList,
+                ]);
+                setCurSession(timeNow);
+                setMessages([]);
+              }, 100);
+            } else {
+              message.error('It is now a new conversation.');
+            }
+          }}
+          className={styles.headerButton}
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+        <Popover
+          placement="bottom"
+          styles={{ body: { padding: 0, maxHeight: 600 } }}
+          content={
+            <Conversations
+              items={sessionList?.map((i) =>
+                i.key === curSession ? { ...i, label: `[current] ${i.label}` } : i,
+              )}
+              activeKey={curSession}
+              groupable
+              onActiveChange={async (val) => {
+                abortController.current?.abort();
+                // The abort execution will trigger an asynchronous requestFallback, which may lead to timing issues.
+                // In future versions, the sessionId capability will be added to resolve this problem.
+                setTimeout(() => {
+                  setCurSession(val);
+                  setMessages(messageHistory?.[val] || []);
+                }, 100);
+              }}
+              styles={{ item: { padding: '0 8px' } }}
+              className={styles.conversations}
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          }
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+          <Button type="text" icon={<CommentOutlined />} className={styles.headerButton} />
+        </Popover>
+        <Button
+          type="text"
+          icon={<CloseOutlined />}
+          onClick={() => setCopilotOpen(false)}
+          className={styles.headerButton}
+        />
+      </Space>
+    </div>
+  );
+  const chatList = (
+    <div className={styles.chatList}>
+      {messages?.length ? (
+        /** 消息列表 */
+        <Bubble.List
+          style={{ height: '100%', paddingInline: 16 }}
+          items={messages?.map((i) => ({
+            ...i.message,
+            classNames: {
+              content: i.status === 'loading' ? styles.loadingMessage : '',
+            },
+            typing: i.status === 'loading' ? { step: 5, interval: 20, suffix: <>💗</> } : false,
+          }))}
+          roles={{
+            assistant: {
+              placement: 'start',
+              footer: (
+                <div style={{ display: 'flex' }}>
+                  <Button type="text" size="small" icon={<ReloadOutlined />} />
+                  <Button type="text" size="small" icon={<CopyOutlined />} />
+                  <Button type="text" size="small" icon={<LikeOutlined />} />
+                  <Button type="text" size="small" icon={<DislikeOutlined />} />
+                </div>
+              ),
+              loadingRender: () => (
+                <Space>
+                  <Spin size="small" />
+                  {AGENT_PLACEHOLDER}
+                </Space>
+              ),
+            },
+            user: { placement: 'end' },
+          }}
+        />
+      ) : (
+        /** 没有消息时的 welcome */
+        <>
+          <Welcome
+            variant="borderless"
+            title="👋 Hello, 我是油菜花 AI 问数助手"
+            description="我可以帮你快速获取数据，并进行分析"
+            className={styles.chatWelcome}
           />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+
+          {/* <Prompts
+            vertical
+            title="I can help："
+            items={MOCK_QUESTIONS.map((i) => ({ key: i, description: i }))}
+            onItemClick={(info) => handleUserSubmit(info?.data?.description as string)}
+            style={{
+              marginInline: 16,
+            }}
+            styles={{
+              title: { fontSize: 14 },
+            }}
+          /> */}
+        </>
+      )}
+    </div>
+  );
+  const sendHeader = (
+    <Sender.Header
+      title="Upload File"
+      styles={{ content: { padding: 0 } }}
+      open={attachmentsOpen}
+      onOpenChange={setAttachmentsOpen}
+      forceRender
+    >
+      <Attachments
+        ref={attachmentsRef}
+        beforeUpload={() => false}
+        items={files}
+        onChange={({ fileList }) => setFiles(fileList)}
+        placeholder={(type) =>
+          type === 'drop'
+            ? { title: 'Drop file here' }
+            : {
+                icon: <CloudUploadOutlined />,
+                title: 'Upload files',
+                description: 'Click or drag files to this area to upload',
+              }
+        }
+      />
+    </Sender.Header>
+  );
+  const chatSender = (
+    <div className={styles.chatSend}>
+      <div className={styles.sendAction}>
+        <Button
+          icon={<ScheduleOutlined />}
+          onClick={() => handleUserSubmit('What has Ant Design X upgraded?')}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          Upgrades
+        </Button>
+        <Button
+          icon={<ProductOutlined />}
+          onClick={() => handleUserSubmit('What component assets are available in Ant Design X?')}
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+          Components
+        </Button>
+        <Button icon={<AppstoreAddOutlined />}>More</Button>
+      </div>
+
+      {/** 输入框 */}
+      <Suggestion items={MOCK_SUGGESTIONS} onSelect={(itemVal) => setInputValue(`[${itemVal}]:`)}>
+        {({ onTrigger, onKeyDown }) => (
+          <Sender
+            loading={loading}
+            value={inputValue}
+            onChange={(v) => {
+              onTrigger(v === '/');
+              setInputValue(v);
+            }}
+            onSubmit={(message) => {
+
+              if (!message.trim()) return;
+
+              
+
+              handleUserSubmit(inputValue);
+              setInputValue('');
+            }}
+            onCancel={() => {
+              abortController.current?.abort();
+            }}
+            allowSpeech
+            placeholder="Ask or input / use skills"
+            onKeyDown={onKeyDown}
+            header={sendHeader}
+            prefix={
+              <Button
+                type="text"
+                icon={<PaperClipOutlined style={{ fontSize: 18 }} />}
+                onClick={() => setAttachmentsOpen(!attachmentsOpen)}
+              />
+            }
+            onPasteFile={onPasteFile}
+            actions={(_, info) => {
+              const { SendButton, LoadingButton, SpeechButton } = info.components;
+              return (
+                <div style={{ display: 'flex', alignItems: 'center', gap: 4 }}>
+                  <SpeechButton className={styles.speechButton} />
+                  {loading ? <LoadingButton type="default" /> : <SendButton type="primary" />}
+                </div>
+              );
+            }}
           />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        )}
+      </Suggestion>
+    </div>
+  );
+
+  useEffect(() => {
+    // history mock
+    if (messages?.length) {
+      setMessageHistory((prev) => ({
+        ...prev,
+        [curSession]: messages,
+      }));
+    }
+  }, [messages]);
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'flex-end', backgroundColor: '#f0f2f5' }}>
+      <div className={styles.copilotChat} style={{ width: copilotOpen ? 450 : 0 }}>
+        {/** 对话区 - header */}
+        {chatHeader}
+
+        {/** 对话区 - 消息列表 */}
+        {chatList}
+
+        {/** 对话区 - 输入框 */}
+        {chatSender}
+      </div>
     </div>
   );
 }
+
+
+const Home = () => {
+  return (
+    <ThreadProvider>
+      <StreamProvider>
+        <Chat />
+      </StreamProvider>
+    </ThreadProvider>
+  );
+};
+
+
+
+export default Home;
---
+++ apps/web-assistant/src/providers/Stream.tsx
@@ -0,0 +1,141 @@
+import React, {
+  createContext,
+  useContext,
+  ReactNode,
+  useState,
+  useEffect,
+} from "react";
+import { useStream } from "@langchain/langgraph-sdk/react";
+import { type Message } from "@langchain/langgraph-sdk";
+import {
+  uiMessageReducer,
+  type UIMessage,
+  type RemoveUIMessage,
+} from "@langchain/langgraph-sdk/react-ui";
+import { useThreads } from "./Thread";
+import { message } from 'antd'
+import { useQueryState } from "nuqs";
+
+export type StateType = { messages: Message[]; ui?: UIMessage[] };
+
+const useTypedStream = useStream<
+  StateType,
+  {
+    UpdateType: {
+      messages?: Message[] | Message | string;
+      ui?: (UIMessage | RemoveUIMessage)[] | UIMessage | RemoveUIMessage;
+    };
+    CustomEventType: UIMessage | RemoveUIMessage;
+  }
+>;
+
+type StreamContextType = ReturnType<typeof useTypedStream>;
+const StreamContext = createContext<StreamContextType | undefined>(undefined);
+
+async function sleep(ms = 4000) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function checkGraphStatus(
+  apiUrl: string,
+  apiKey: string | null,
+): Promise<boolean> {
+  try {
+    const res = await fetch(`${apiUrl}/info`, {
+      ...(apiKey && {
+        headers: {
+          "X-Api-Key": apiKey,
+        },
+      }),
+    });
+
+    return res.ok;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+}
+
+const StreamSession = ({
+  children,
+  apiKey,
+  apiUrl,
+  assistantId,
+}: {
+  children: ReactNode;
+  apiKey: string | null;
+  apiUrl: string;
+  assistantId: string;
+}) => {
+  const [threadId, setThreadId] = useQueryState("threadId");
+  const { getThreads, setThreads } = useThreads();
+  const streamValue = useTypedStream({
+    apiUrl,
+    apiKey: apiKey ?? undefined,
+    assistantId,
+    threadId: threadId ?? null,
+    onCustomEvent: (event, options) => {
+      options.mutate((prev) => {
+        const ui = uiMessageReducer(prev.ui ?? [], event);
+        return { ...prev, ui };
+      });
+    },
+    onThreadId: (id) => {
+      setThreadId(id);
+      // Refetch threads list when thread ID changes.
+      // Wait for some seconds before fetching so we're able to get the new thread that was created.
+      sleep().then(() => getThreads().then(setThreads).catch(console.error));
+    },
+  });
+
+  useEffect(() => {
+    checkGraphStatus(apiUrl, apiKey).then((ok) => {
+      if (!ok) {
+        message.error(
+          `Failed to connect to LangGraph server
+            <p>
+              Please ensure your graph is running at <code>{apiUrl}</code> and
+              your API key is correctly set (if connecting to a deployed graph).
+            </p>
+          `, 
+       );
+      }
+    });
+  }, [apiKey, apiUrl]);
+
+  return (
+    <StreamContext.Provider value={streamValue}>
+      {children}
+    </StreamContext.Provider>
+  );
+};
+
+// Default values for the form
+const DEFAULT_API_URL = "http://localhost:2024";
+const DEFAULT_ASSISTANT_ID = "agent";
+
+export const StreamProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  // Get environment variables
+  const envApiUrl = process.env.NEXT_PUBLIC_API_URL ?? DEFAULT_API_URL ?? "";
+  const envAssistantId = process.env.NEXT_PUBLIC_ASSISTANT_ID ?? DEFAULT_ASSISTANT_ID ?? "";
+  const envApiKey = process.env.NEXT_PUBLIC_LANGSMITH_API_KEY ?? "";
+
+  return (
+    <StreamSession apiKey={envApiKey} apiUrl={envApiUrl} assistantId={envAssistantId}>
+      {children}
+    </StreamSession>
+  );
+};
+
+// Create a custom hook to use the context
+export const useStreamContext = (): StreamContextType => {
+  const context = useContext(StreamContext);
+  if (context === undefined) {
+    throw new Error("useStreamContext must be used within a StreamProvider");
+  }
+  return context;
+};
+
+export default StreamContext;
